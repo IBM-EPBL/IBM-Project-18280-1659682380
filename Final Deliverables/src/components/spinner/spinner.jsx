1096c4333f599b98a3c20208867fd7c14ccc6d56
Abishek PS <68456941+a2bshek@users.noreply.github.com>
2022-11-20 13:40:40 +0530

    Delete Final Deliverables directory

--- Final Deliverables/src/components/spinner/spinner.jsx
+++
@@ -1,8 +0,0 @@
-import { SpinnerOverlay, SpinnerContainer } from "./spinner.styles";
-
-const Spinner = ()=>
-    <SpinnerOverlay>
-        <SpinnerContainer />
-    </SpinnerOverlay>
-
-export default Spinner;
